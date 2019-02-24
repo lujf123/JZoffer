7e1e928f16996761e65d6c4dc635f35043deaf72
jeffrey <729957621@qq.com>
2019-02-24 17:07:30 +0800

    day9

---
+++ src/main/java/other/FileSystemClassLoader.java
@@ -0,0 +1,52 @@
+package other;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
+
+public class FileSystemClassLoader extends ClassLoader {
+
+    private String rootDir;
+
+    public FileSystemClassLoader(String rootDir) {
+        this.rootDir = rootDir;
+    }
+
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] classData = getClassData(name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        } else {
+            return defineClass(name, classData, 0, classData.length);
+        }
+    }
+
+    private byte[] getClassData(String className) {
+        String path = classNameToPath(className);
+        try {
+            InputStream ins = new FileInputStream(className);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int bytesNumRead;
+            while ((bytesNumRead = ins.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesNumRead);
+            }
+            return baos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String classNameToPath(String className) {
+        return rootDir + File.separatorChar + className.replace('.', File.separatorChar) + ".class";
+    }
+
+    public static void main(String[] args) {
+        Integer i = new Integer(3);
+        Class<Integer> c = Integer.class;
+    }
+
+
+}
