c2bdd6548ebf1d412b818143baa4cbaeb5c235a2
jeffrey <729957621@qq.com>
2019-03-05 22:52:18 +0800

    day13

---
+++ src/main/java/huawei/Test.java
@@ -0,0 +1,17 @@
+package huawei;
+
+import java.util.*;
+
+/**
+ * Author: 王俊超
+ * Date: 2016-01-05 14:49
+ * All Rights Reserved !!!
+ */
+public class Test {
+    public static void main(String[] args) {
+        char c = 'z';
+        System.out.println(c == 'z');
+    }
+
+
+}
