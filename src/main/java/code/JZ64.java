af557807661eee1210319ed9ec8910ea731f35c3
jeffrey <729957621@qq.com>
2019-03-29 15:04:06 +0800

    day20

---
+++ src/main/java/code/JZ64.java
@@ -0,0 +1,9 @@
+package code;
+
+public class JZ64 {
+    public int Sum_Solution(int n) {
+        int sum = n;
+        boolean b = (n > 0) && ((sum += Sum_Solution(n-1)) > 0);
+        return sum;
+    }
+}
